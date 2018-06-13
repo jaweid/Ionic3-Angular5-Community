111c9c8c2bdfa4696c38aff596979148ca0c5847
liujiawei <140336038@qq.com>
2018-06-13 14:57:24 +0800

    init

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = 'HomePage';
+  tab2Root = 'SortPage';
+  tab3Root = 'PostPage';
+  tab4Root = 'OrderPage';
+  tab5Root = 'MinePage';
 
   constructor() {
 
